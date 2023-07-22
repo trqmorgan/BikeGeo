6c5edfe88e0c9e1845aa26930d4b3d94f57204e9
trqmorgan@gmail.com <trqmorgan@gmail.com>
2023-07-22 18:43:15 +0100

    Updating views to show tables

--- app/bike_geometry/urls.py
+++ app/bike_geometry/urls.py
@@ -4,8 +4,7 @@ from . import views
 
 app_name = "bike_geometry"
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path('list/', views.bike_list, name='bike_list'),
-    path('search/', views.search, name='search'),
-   
+    path("", views.index, name="bike_list"),
+    path("search/", views.search, name="search"),
+    path("search/<int:id>/", views.bike_detail, name="detail"),
 ]
